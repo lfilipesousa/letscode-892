e1dc28049cfe52ebc341edb9a1dae333bed09fb1
Luiz Batista <l.filipesousa47@gmail.com>
2022-11-07 17:08:09 -0300

    Adicionando a funcao subtracao

--- Tecnicas-de-programacao/Projetos/calculadora/calculadora/funcoes.py
+++ Tecnicas-de-programacao/Projetos/calculadora/calculadora/funcoes.py
@@ -11,3 +11,17 @@ def soma(a, b):
         return a + b
     else:
         raise TypeError(f'O input "a" e "b" devem ser um número, recebido {a} ({type(a)}), {b} ({type(b)})') 
+
+def subtracao(a, b):
+        '''
+    args:
+        a: (int, float)
+        b: (int, float)
+    return:
+        a + b (int,float)
+    '''
+
+    if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        return a - b
+    else:
+        raise TypeError(f'O input "a" e "b" devem ser um número, recebido {a} ({type(a)}), {b} ({type(b)})') 
