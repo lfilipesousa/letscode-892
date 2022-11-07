0f110306410f8b601ffda4b4b3045f425459c518
Luiz Batista <l.filipesousa47@gmail.com>
2022-11-07 17:18:50 -0300

    Adicionando funcao de multiplicacao

--- Tecnicas-de-programacao/Projetos/calculadora/calculadora/funcoes.py
+++ Tecnicas-de-programacao/Projetos/calculadora/calculadora/funcoes.py
@@ -1,4 +1,4 @@
-def soma(a, b):
+def subtracao(a, b):
     '''
     args:
         a: (int, float)
@@ -9,5 +9,19 @@ def soma(a, b):
 
     if isinstance(a, (int, float)) and isinstance(b, (int, float)):
         return a + b
+    else:
+        raise TypeError(f'O input "a" e "b" devem ser um número, recebido {a} ({type(a)}), {b} ({type(b)})') 
+
+def multiplicacao(a,b):
+    '''
+    args:
+        a: (int, float)
+        b: (int, float)
+    return:
+        a * b (int,float)
+    '''
+
+    if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        return a * b
     else:
         raise TypeError(f'O input "a" e "b" devem ser um número, recebido {a} ({type(a)}), {b} ({type(b)})') 
