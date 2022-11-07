e5b98bf7863735d9e57adf5529802324812a4df5
Luiz Batista <l.filipesousa47@gmail.com>
2022-11-07 17:34:39 -0300

    Adicionando a funcao divisao

--- Tecnicas-de-programacao/Projetos/calculadora/calculadora/funcoes.py
+++ Tecnicas-de-programacao/Projetos/calculadora/calculadora/funcoes.py
@@ -25,3 +25,22 @@ def subtracao(a, b):
         return a - b
     else:
         raise TypeError(f'O input "a" e "b" devem ser um número, recebido {a} ({type(a)}), {b} ({type(b)})') 
+
+
+def divisao (a, b):
+    '''
+    args:
+        a: (int, float)
+        b: (int, float)
+    return:
+        a / b (int,float)
+    '''
+
+    if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        return a / b
+    else:
+        raise TypeError(f'O input "a" e "b" devem ser um número, recebido {a} ({type(a)}), {b} ({type(b)})') 
+
+    if b == 0:
+        print('O número não pode ser dividido por 0')
+        return 0
